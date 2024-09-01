ce567fce0b9697c7a9dec5a2300fa8839431c6d5
miiiiyaaaa <pissenlit.2624@gmail.com>
2024-09-01 11:16:31 +0900

    Add files via upload

---
+++ kadai_001/main.js
@@ -0,0 +1,15 @@
+$(function () {
+
+    //カルーセル
+    $('.carousel').slick ({
+    autoplay: true,
+    dots: true,
+    fade: true,
+    infinite: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+
+    });
+
+});
+
